d860b3e3f479efa729af3942aff1b39d79c465e3
iareuniqe <1062698930@qq.com>
2018-06-21 00:55:16 +0800

    第一次提交

---
+++ jvm/src/com/tech/jvm/HeapOOM.java
@@ -0,0 +1,12 @@
+package com.tech.jvm;
+
+/**
+ * @author xxx_xx
+ * @date 2018/6/21
+ */
+public class HeapOOM {
+
+    public static void main(String[] args) {
+        System.out.println("xxxxxxxxxx");
+    }
+}
